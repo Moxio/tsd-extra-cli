10eae1ffa0b0e4602bc657cb48c97645c60dceab
Arnout Boks <arnout.boks@moxio.com>
2020-09-30 11:29:32 +0200

    Add project-path CLI argument (like TSD itself)

--- bin/index.js
+++ bin/index.js
@@ -5,9 +5,18 @@
 
 const tsd = require("tsd").default;
 const tsdFormatter = require("tsd/dist/lib/formatter").default;
+const yargs = require("yargs");
+
+const argv = yargs
+    .usage('Usage: $0 [path]')
+    .argv;
+
+const projectPath = argv._[0] || process.cwd();
 
 (async () => {
-    const diagnostics = await tsd();
+    const diagnostics = await tsd({
+        cwd: projectPath,
+    });
 
     if (diagnostics.length > 0) {
         console.error(tsdFormatter(diagnostics));
