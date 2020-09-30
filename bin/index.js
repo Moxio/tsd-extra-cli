c631a248e9327178894c5d111dc818013599810d
Arnout Boks <arnout.boks@moxio.com>
2020-09-30 12:44:53 +0200

    Add option to display only errors found by tests
    By default, TSD also displays TypeScript errors found in the code
    of the system under test. When using the --tests-only flag, those
    are ignored. This can be useful when introducing tests into a
    codebase that has known TypeScript errors.

--- bin/index.js
+++ bin/index.js
@@ -9,14 +9,23 @@ const yargs = require("yargs");
 
 const argv = yargs
     .usage('Usage: $0 [path]')
+    .option('tests-only', { type: 'boolean', describe: 'Only consider errors in tests, not TypeScript errors in the system under test' })
     .argv;
 
 const projectPath = argv._[0] || process.cwd();
 
+function isTestFile(file) {
+    return file.endsWith('.test-d.ts');
+}
+
 (async () => {
-    const diagnostics = await tsd({
+    let diagnostics = await tsd({
         cwd: projectPath,
     });
+
+    if (argv['tests-only']) {
+        diagnostics = diagnostics.filter((diagnostic) => isTestFile(diagnostic.fileName));
+    }
 
     if (diagnostics.length > 0) {
         console.error(tsdFormatter(diagnostics));
